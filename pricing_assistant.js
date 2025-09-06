621bb5455b4a8d65dd8800af2d74b5e17586d51c
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-06 16:25:40 -0400

    first commit

---
+++ pricing_assistant.js
@@ -0,0 +1,21 @@
+// Coding Challenge 02a
+
+let productName = "Vacuuum Cleaner";
+let costPerUnit = 449.99;
+let basePrice = 499.99;
+let discountRate = 0.1;
+let salesTaxRate = 0.05;
+fixedMonthlyCosts = 1999.99;
+
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax  = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+console.log("Product name:", productName);
+console.log("Discounted price (before tax): $", discountedPrice.toFixed(2));
+console.log("Final price with tax: $", finalPriceWithTax.toFixed(2));
+console.log("Profit per unit: $", profitPerUnit.toFixed(2));
+console.log("Break-even units: ", breakEvenUnits);
+console.log("Per-unit profitablity:", isProfitablePerUnit);

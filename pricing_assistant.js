de5f6db1f468517cfb031d081977e0f1866eb454
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-06 16:33:27 -0400

    d

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -5,7 +5,7 @@ let costPerUnit = 449.99;
 let basePrice = 499.99;
 let discountRate = 0.1;
 let salesTaxRate = 0.05;
-fixedMonthlyCosts = 1999.99;
+let fixedMonthlyCosts = 1999.99;
 
 let discountedPrice = basePrice * (1 - discountRate);
 let finalPriceWithTax  = discountedPrice * (1 + salesTaxRate);
